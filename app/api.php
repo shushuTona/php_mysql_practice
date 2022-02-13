1b76fe4d6c23d8c0c7710f9706f274d4acf5235b
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-02-13 15:24:40 +0000

    fixed routing file

--- app/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('PHP_ROOT_DIR', '/var/www/php_mysql_practice/app/');
-
-    if ($_SERVER['REQUEST_URI'] === '/api/hoge') {
-        require_once(PHP_ROOT_DIR . 'index.php');
-    }
--- app/index.php
+++ app/index.php
@@ -1,10 +1,15 @@
 <?php
-    header('Content-Type: application/json; charset=UTF-8');
+    define('APPLICATION_ROOT_DIR', '/var/www/php_mysql_practice/app/');
+    define('ROUTES_ROOT_DIR', '/var/www/php_mysql_practice/app/routes/');
 
-    $result = [
-        'hoge' => 100,
-        'piyo' => 200,
-        'fuga' => 300,
-    ];
+    require_once(APPLICATION_ROOT_DIR . 'env.php');
 
-    echo json_encode($result, JSON_UNESCAPED_SLASHES);
+    $request_uri = $_SERVER['REQUEST_URI'];
+
+    if(
+        preg_match('/^\/api\//', $request_uri)
+    ) {
+        require_once(ROUTES_ROOT_DIR . 'api.php');
+    } else {
+        require_once(ROUTES_ROOT_DIR . 'web.php');
+    }
